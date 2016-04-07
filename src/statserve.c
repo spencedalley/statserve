dce16e7c5b6120c632e586ee7d28e90fa063cbc4
spence dalley <spencedalley@users.noreply.github.com>
2016-04-06 18:29:13 -0700

    Skeleton of an echo server

--- src/statserve.c
+++ src/statserve.c
@@ -2,7 +2,21 @@
 #include <ctype.h>
 #include "dbg.h"
 
-int echo_server()
+int echo_server(const char *host, const char *port)
 {
+	check(host != NULL, "Invalid host."); 
+	check(port != NULL, "Invalid port."); 
+	
+	// Create a sigaction that handles SIGCHLD
+	// listen on the given port and host
+
+	while(1) {
+		// accept a client connection 
+		// fork process
+		// if 0 then child process
+		// else server
+	}
+
+error: // fallthrough
     return -1;
 }
